f89897571b039ed81e42bb1548979ac7ecf3ac4e
steffy-lo <steffy-lo@hotmail.com>
2022-06-25 21:47:29 +0800

    updated gitignore and build script

--- dist/contentScript.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-  const contentMain = await import(chrome.runtime.getURL("main.js"));
-  const { issueKeys, prNum, prLink } = contentMain.main();
-  chrome.storage.sync.set({
-      [prNum]: JSON.stringify([...issueKeys]),
-      prLink
-  })
-})();
