67d0ea399347a2f55416385fa534f657875ea2a1
steffy-lo <steffy-lo@hotmail.com>
2022-06-27 11:23:12 +0800

    minor styling fixes

--- src/popup.js
+++ src/popup.js
@@ -17,7 +17,7 @@ const addNewIssue = (issues, issue) => {
         <h1 class="title-font text-lg font-medium text-gray-900 mb-3">${summary || "No summary"}</h1>
         <p class="leading-relaxed mb-3">${description || "No description"}</p>
         <div class="flex items-center flex-wrap ">
-          <a href=${issueLink} target="_blank" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">View Issue
+          <a href=${issueLink} target="_blank" class="text-blue-600 inline-flex items-center md:mb-2 lg:mb-0">View Issue
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
